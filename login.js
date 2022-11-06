4ad1468a2898b5caf7e613e7fb81881ff56fef7f
Gabriel de Lima <114449624+LimaKsp@users.noreply.github.com>
2022-11-06 15:06:53 -0300

    Add files via upload
    teste novo background, e validações do formulário

--- login.js
+++ login.js
@@ -1,10 +1,19 @@
-console.log(111)
+let imagemTroca = document.querySelector("#imagemTroca")
+let botoesSome = document.querySelector(".botoes")
+let navBar = document.querySelector(".nav1")
 
 function abrir() {
     if (itens.style.display == 'flex') {
         itens.style.display = 'none'
+        imagemTroca.setAttribute("src", "menu.png")
+        botoesSome.style.display = 'flex'
+        navBar.style.borderBottom = "none"
+
     } else {
         itens.style.display = 'flex'
+        imagemTroca.setAttribute("src", "menu-x.png")
+        botoesSome.style.display = 'none'
+        navBar.style.borderBottom = "dashed 3px white"
     }
 }
 function mudarTamanho() {
@@ -12,3 +21,24 @@ function mudarTamanho() {
         itens.style.display = 'flex'
     }
 }
+
+
+
+/* validação para o formulario de login */
+
+function validateform(){  
+  // var Email= document.myform.Emailt.value;  
+  var password=document.myform.password.value;  
+  var x=document.myform.Email.value;  
+  var atposition=x.indexOf("@");  
+  var dotposition=x.lastIndexOf(".");  
+  
+  if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+    alert("Por favor,digite um Email válido");  
+    return false; 
+  }else if(password.length<6){  
+    alert("Por favor,confira sua Senha");  
+    return false;  
+    }  
+  } 
+  
